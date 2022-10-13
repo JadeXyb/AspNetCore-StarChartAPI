b27cc91bf7ae2c4102bc2c48647ffc328aded8ae
JadeXyb <111397175+JadeXyb@users.noreply.github.com>
2022-10-13 13:44:21 +0100

    NotMapped Satellites

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,7 @@ namespace StarChart.Models
 
         public int? OrbitedObjectId { get; set; }
 
+        [NotMapped]
         public List<CelestialObject> Satellites { get; set; }
 
         public TimeSpan OrbitalPeriod { get; set; }
