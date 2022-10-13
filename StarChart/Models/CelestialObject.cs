ed870e293ba57f791f817dffb6482fb12f33c28e
JadeXyb <111397175+JadeXyb@users.noreply.github.com>
2022-10-13 13:41:03 +0100

    Create CelestialObject Model

---
+++ StarChart/Models/CelestialObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarChart.Models
+{
+    public class CelestialObject
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+
+        public int? OrbitedObjectId { get; set; }
+    }
+}
